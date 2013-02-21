c952fa4f4fbb204ee1cce43e64eb2456393a8cd0
Eric Davis <endavis@gmail.com>
2013-02-21 18:48:57 +0000

    utils.py  - add function to convert unicode to ansi

--- libs/utils.py
+++ libs/utils.py
@@ -61,4 +61,15 @@ def verify(val, vtype):
   else:
     return vtype(val)
   
+def convert(input):
+  """converts input to ascii"""  
+  if isinstance(input, dict):
+      return {convert(key): convert(value) for key, value in input.iteritems()}
+  elif isinstance(input, list):
+      return [convert(element) for element in input]
+  elif isinstance(input, unicode):
+      return input.encode('utf-8')
+  else:
+      return input
+    
   
