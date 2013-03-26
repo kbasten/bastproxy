7264b982175369711f18927c5045ca01e4c6986e
Eric Davis <endavis@gmail.com>
2013-03-26 00:08:15 +0000

    utils.py  - new: added some time and number functions

--- libs/utils.py
+++ libs/utils.py
@@ -4,6 +4,7 @@ $Id$
 import fnmatch
 import os
 import datetime
+import math
 from libs.color import iscolor
 
 
@@ -61,6 +62,87 @@ def timedeltatostring(stime, etime, fmin=False, colorn='', colors=''):
   out   = ":".join(tmsg)
   return out
 
+
+def ReadableNumber(num, places=2):
+  """
+  convert a number to a shorter readable number
+  """
+  ret = ''
+  nform = "%%00.0%sf" % places
+  if not num:
+      return 0
+  elif num >= 1000000000000:
+      ret = nform % (num / 1000000000000.0) + " T" # trillion
+  elif num >= 1000000000:
+      ret = nform % (num / 1000000000.0) + " B" # billion
+  elif num >= 1000000:
+      ret = nform % (num / 1000000.0) + " M" # million
+  elif num >= 1000:
+      ret = nform % (num / 1000.0) + " K" # thousand
+  else:
+      ret = num # hundreds
+  return ret
+
+
+def SecondsToDHMS(sseconds):
+  """
+  convert seconds to years, days, hours, mins, secs
+  """
+  nseconds = int(sseconds)
+  dtime = {
+      'years' : 0,
+      'days' : 0,
+      'hours' : 0,
+      'minutes': 0,
+      'seconds': 0 
+      }
+  if nseconds == 0:
+    return dtime
+  
+  dtime['years'] = math.floor(nseconds/(3600 * 24 * 365))
+  dtime['days'] = math.floor(nseconds/(3600 * 24))
+  dtime['hours'] = math.floor(nseconds/3600 - (dtime['days'] * 24))
+  dtime['mins'] = math.floor(nseconds/60 - (dtime['hours'] * 60) \
+                                        - (dtime['days'] * 24 * 60))
+  dtime['secs'] = nseconds % 60
+  return dtime
+  
+  
+def format_time(length, nosec=False):
+  """
+  format a length of time into a string
+  """
+  msg = []
+  dtime = SecondsToDHMS(length)
+  years = False
+  days = False
+  hours = False
+  mins = False
+  if dtime['years'] > 0:
+    years = True
+    msg.append('%dy' % (dtime['years'] or 0))
+  if dtime['days'] > 0:
+    if years:
+      msg.append(':')
+    days = True
+    msg.append('%dd' % (dtime['days'] or 0))
+  if dtime['hours']:
+    if years or days:
+      msg.append(':')
+    hours = True
+    msg.append('%dh' % (dtime['hours'] or 0))
+  if dtime['mins'] > 0:
+    if years or days or hours:
+      msg.append(':')
+    mins = True
+    msg.append('%dm' % (dtime['mins'] or 0))
+  if (dtime['secs'] > 0 or len(msg) == 0) and not nosec:
+    if years or days or hours or mins:
+      msg.append(':')
+    msg.append('%ds' % (dtime['secs'] or 0))
+  
+  return ''.join(msg)
+
   
 def verify_bool(val):
   """
@@ -78,6 +160,7 @@ def verify_bool(val):
       return True
   
   return bool(val)
+
 
 def verify_color(val):
   """
@@ -115,5 +198,4 @@ def convert(tinput):
     return tinput.encode('utf-8')
   else:
     return tinput
-  
   
